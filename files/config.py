595f2e4accb6dc5e67680cd0eb6132ed7badbc99
Erbol <99820539+erbol-alauaddinov@users.noreply.github.com>
2025-03-13 11:53:48 +0500

    Update config.py

--- files/config.py
+++ files/config.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 
 API_ID = 1152318
 API_HASH = "091250487383dca290c80a37c1ee16e7"
-API_TOKEN = "7777345494:AAFFW_nMGEFMg1Od5oIqsMBob7GkYZkkDx4"
+API_TOKEN = "7586784231:AAFUdKEy_ype6a4epDpBPlTRt8xrVjRI0IU"
 app = Client("Music", api_id=API_ID, api_hash=API_HASH, bot_token=API_TOKEN)
 admin=5919321514
 bot="einsta_bot"
